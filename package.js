05fb6f04fb0850f907840238eb0b1f0735a3be11
zhouzhuojie <zhouzhuojie@gmail.com>
2014-09-04 12:05:08 -0400

    Update api.export

--- package.js
+++ package.js
@@ -9,6 +9,7 @@ Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0.1');
   api.addFiles('./kramed/kramed.min.js', 'client');
   api.addFiles('rexzh0u:kramed.js', 'client');
+  api.export('kramed', 'client');
 });
 
 Package.onTest(function(api) {
