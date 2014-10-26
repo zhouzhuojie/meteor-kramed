af3b4ad2410c21fb85bdc332d44bee89611a3443
zhouzhuojie <zhouzhuojie@gmail.com>
2014-10-26 11:17:40 -0400

    Update dependencies, simple:katex@1.0.5

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   summary: "A markdown (kramdown compatible) parser and compiler.",
-  version: "1.0.0",
+  version: "1.0.1",
   git: "https://github.com/zhouzhuojie/meteor-kramed.git",
   name: "rexzh0u:kramed"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0.1');
-  api.use('sashko:katex@1.0.2', {weak: true});
+  api.use('simple:katex@1.0.5', {weak: true});
   api.addFiles('./kramed/kramed.min.js');
   api.addFiles('rexzh0u:kramed.js');
   api.export('kramed');
@@ -16,6 +16,6 @@ Package.onUse(function(api) {
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('rexzh0u:kramed');
-  api.use('sashko:katex@1.0.2');
+  api.use('simple:katex@1.0.5');
   api.addFiles('rexzh0u:kramed-tests.js');
 });
